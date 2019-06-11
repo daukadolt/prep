5d2053bf43c250cd3c789ecda89fee6330247289
daukadolt <daulet.amirkhanov.official@gmail.com>
2019-06-11 17:00:31 +0600

    fixed heapsort

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -1,10 +1,24 @@
-from heapq import heapify, heappop
+def heapify(arr, currentIndex, boundary):
+    leftChildIndex = currentIndex*2 + 1
+    rightChildIndex = currentIndex*2 + 2
+    leftChild = float("-inf") if leftChildIndex>=len(arr) or leftChildIndex>boundary else arr[leftChildIndex]
+    rightChild = float("-inf") if rightChildIndex>=len(arr) or rightChildIndex>boundary else arr[rightChildIndex]
+    largest = max(leftChild, rightChild)
+    largestIndex = leftChildIndex if leftChild == largest else rightChildIndex
+    if largest != float("-inf") and largest > arr[currentIndex]:
+        arr[currentIndex], arr[largestIndex] = arr[largestIndex], arr[currentIndex]
+        buildMaxHeap(arr, largestIndex, boundary)
+
+def buildMaxHeap(arr, begin, end):
+    heapSize = end-begin
+    for i in range(heapSize//2, -1, -1):
+        heapify(arr, i, end)
 
 def heapsort(arr):
-	copy = [num for num in arr]
-	heapify(copy)
-	for i in range(len(arr)):
-		arr[i] = heappop(copy)
+    for i in range(len(arr)-1, -1, -1):
+        buildMaxHeap(arr, 0, i)
+        arr[0], arr[i] = arr[i], arr[0]
+    return arr
 
 from random import randint
 
